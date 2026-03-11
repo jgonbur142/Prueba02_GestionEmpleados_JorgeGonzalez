18f615c46d32817c12a11f63cfb37feaa31b6f3e
jgonbur142 <jgonbur142@g.educaand.es>
2026-03-11 11:01:21 +0100

    commit final de tercera hora

--- src/app/GestorEmpleados.java
+++ src/app/GestorEmpleados.java
@@ -22,41 +22,61 @@ public class GestorEmpleados {
 			opcion=sc.nextLine();
 			
 			switch (opcion) {
-			case "1"-> {
-				String opcion2="";
-				consola.imprimirLinea("Qué tipo de empleado quiere contratar: \n1 - Técnico\\n 2 - Comercial\n");
-				switch(opcion2) {
-				case "1"->{
-					//solicitar datos Técnico
+				case "1"-> {
+					contratarEmpleado();
 				}
 				case "2"->{
-					//solicitar datos Comercial
+					listarTodos();
+				}
+				case"3"->{
+					listarPorFiltro();
+				}
+				case"4"->{
+					consola.limpiarPantalla();
+					
 				}
 				default->{
 					consola.imprimir("Opción no válida");
 				}
-				}
-			}
-			case "2"->{
-				
-			}
-			case"3"->{
-				consola.imprimirLinea("Introduce un nombre o apellido para filtrar empleados: ");
-				plantilla.getEmpleadoPorNombre("");
-			}
-			case"4"->{
-				
-			}
-			default->{
-				consola.imprimir("Opción no válida");
-			}
 			}
 		}while(!opcion.equals("4"));
 		
 	}
 	
 	private void contratarEmpleado() {
+		Scanner sc = new Scanner(System.in);
+		String opcion2="";
 		
+//		String dni, nombre, apellidos;
+//		double sueldoBase;
+//		int categoria;
+		consola.imprimirLinea("Qué tipo de empleado quiere contratar: \n1 - Técnico\\n 2 - Comercial\n");
+		switch(opcion2) {
+			case "1"->{
+				//solicitar datos Técnico
+				consola.imprimirLinea("Introduce los datos del Técnico: ");
+				//Empleado tecnico = new Tecnico(sc.nextLine(), sc.nextLine(), sc.nextLine(), sc.nextDouble(), sc.nextInt());
+//				consola.imprimir("DNI: ");
+//				dni = sc.nextLine();
+//				consola.imprimir("Nombre: ");
+//				nombre = sc.nextLine();
+//				consola.imprimir("Apellidos: ");
+//				apellidos = sc.nextLine();
+//				consola.imprimir("Sueldo base: ");
+//				sueldoBase = sc.nextDouble();
+//				consola.imprimir("Categoría: ");
+//				categoria = sc.nextInt();
+				
+			}
+			case "2"->{
+				//solicitar datos Comercial
+				consola.imprimirLinea("Introduce los datos del Comercial: ");
+				consola.imprimir("DNI: ");
+			}
+			default->{
+				consola.imprimir("Opción no válida");
+			}
+		}
 	}
 	
 	private void listarTodos() {
@@ -64,11 +84,18 @@ public class GestorEmpleados {
 	}
 	
 	private void listarPorFiltro() {
+		Scanner sc = new Scanner(System.in);
 		
+		consola.imprimirLinea("Introduce un nombre o apellido para filtrar empleados: ");
+		plantilla.getEmpleadoPorNombre(sc.nextLine());
+		
+		//listarEmpleados(empleados);
+	
 	}
 	
-	private void listarEmpleado() {
+	private void listarEmpleados(List<Empleado> empleados) {
 		
+		System.out.println(empleados);
 	}
 	
 	private void ordenarPorNombre(List<Empleado> empleados) {
--- src/app/Main.java
+++ src/app/Main.java
@@ -2,8 +2,16 @@ package app;
 
 public class Main {
 
+	public void show() {
+		Consola consola = new Consola();
+		Plantilla plantilla = new Plantilla();
+		GestorEmpleados gestor = new GestorEmpleados(plantilla, consola);
+		
+		gestor.ejecutar();
+	}
+	
 	public static void main(String[] args) {
-
+		new Main().show();
 	}
 
 }
